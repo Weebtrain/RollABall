c1f5765b4914f454a9ac49045a51f00459ee1c17
Weebtrain <156955415+Weebtrain@users.noreply.github.com>
2025-01-25 09:41:21 -0800

    RollABall part 6.3
    Added vector2s together

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 
 public class BallController : MonoBehaviour
@@ -5,27 +6,30 @@ public class BallController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 inputVector = Vector2.zero; // intialize our input vector
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("User's Input: W");
+            inputVector += Vector2.up;
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("User's Input: A");
-        }
+        
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("User's Input: S");
+            inputVector += Vector2.down;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("User's Input: D");
+            inputVector += Vector2.right;
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+        Debug.Log("Resultant Vector: " + inputVector);
     }
 }
