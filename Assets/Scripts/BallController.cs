73c36cf79274f45369a9c3c4c80342a36332be8b
Weebtrain <156955415+Weebtrain@users.noreply.github.com>
2025-01-25 10:12:47 -0800

    RollABall part 6.4
    Added movement with slight variations from tutorial as converting from vector2 to vector3 was pointless

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,8 +1,9 @@
-
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public Rigidbody ballRigidbody;
+    public float ballSpeed = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,24 +13,23 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero; // intialize our input vector
+        Vector3 inputVector = Vector3.zero; // intialize our input vector
         if (Input.GetKey(KeyCode.W))
         {
-            inputVector += Vector2.up;
+            inputVector += Vector3.forward;
         }
-        
         if (Input.GetKey(KeyCode.S))
         {
-            inputVector += Vector2.down;
+            inputVector += Vector3.back;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            inputVector += Vector2.right;
+            inputVector += Vector3.right;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            inputVector += Vector2.left;
+            inputVector += Vector3.left;
         }
-        Debug.Log("Resultant Vector: " + inputVector);
+        ballRigidbody.AddForce(inputVector * ballSpeed);
     }
 }
